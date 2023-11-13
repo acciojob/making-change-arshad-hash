a61738f59407900f5b24a2d6f18689a4070c2de8
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-13 15:10:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-const makeChange = (c) => 
+const makeChange = (c) => {
 	//created an object
 	let result  ={
 	q:0,
 	d:0,
 	n:0, 
 	p: 0,
-}
+};
 //i need to devide that with 1,
 //5 , 10 , 25 
 	while (c>0) {
-	if(c%25==0)
-		
-		{c= c/25;
+	if(c>=25)
+		//
+		{c= c-25;
 			result.q++;}
-
-		else if(c%10==0)
+//23
+		else if(c>=10)
 		{
-			c=c/10;
+			c=c-10;
+            //c=13
+            //c=3
 			result.d++;
+            //d=1
+            //d=2
 		}
-		else if(c%5==0)
+		else if(c>=5)
 		{
-			c=c/5;
-			result.n++;
+			c=c-5;
+			result.n++; 
 			
 		}
 		else{
 			c=c-1;
-			result.d++;
+			result.p++;
 		}
 		
 		
